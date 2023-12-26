f4e2b2902c348ccd8b4e93bb9165b3ada9dd45ff
Kev <87592672+0xkev21@users.noreply.github.com>
2023-12-26 16:07:36 +0630

    Add a minmax algorithm to make the bot unbeatable

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function Gameboard () {
 
     const printBoard = () => {
         const boardWithValues = board.map(cell => cell.getValue());
-        console.log(boardWithValues);
         return boardWithValues;
     }
 
@@ -58,60 +57,26 @@ function GameController(
 
     const board = Gameboard();
 
-    let bot1, bot2;
-
-    if(isPlayerOneBot) {
-        bot1 = Bot('X', board);
-    }
-    if(isPlayerTwoBot) {
-        bot2 = Bot('O', board);
-    }
+    let bot;
 
     let activePlayer = players[0];
 
-    const getActivePlayer = () => activePlayer;
-
     let winner = null;
+
+    const getActivePlayer = () => activePlayer;
 
     const getWinner = () => winner;
 
     const switchActivePlayer = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
-        if(activePlayer.isBot) {
-            playBotMove();
-        }
-    }
-
-    function Bot (marker) {
-
-        const botMarker = marker;
-    
-        const makeMove = (origBoard) => {
-            const emptyCells = origBoard.getEmptyCells().map(cell => cell.getValue());
-            if(emptyCells.length > 0) {
-                const randomIndex = Math.floor(Math.random() * emptyCells.length);
-                console.log(emptyCells);
-                return emptyCells[randomIndex];
-            }
-            return null;
-        }
-        return {makeMove};
-    }
-
-    const playBotMove = () => {
-        if (bot1) {
-            const move = bot1.makeMove(board);
-            if(move) playRound(move);
-        } else if(bot2) {
-            const move = bot2.makeMove(board);
-            if(move) playRound(move);
-        }
     }
 
     const printNewRound = () => {
         console.log(`${getActivePlayer().name}'s turn.`);
         board.printBoard();
-        if(activePlayer.isBot) return;
+        if(activePlayer.isBot) {
+            playBotMove();
+        }
     }
 
     const playRound = (cell) => {
@@ -119,7 +84,7 @@ function GameController(
 
         console.log(`Adding ${getActivePlayer().name}'s mark at cell ${cell}.`);
 
-        const result = checkWin(board);
+        const result = checkWin(board.printBoard(), getActivePlayer().marker);
         handleWin(result);
         if(!result) {
             switchActivePlayer();
@@ -129,12 +94,10 @@ function GameController(
         printNewRound();
     }
 
-    const checkWin = (board) => {
-        const boardWithValues = board.printBoard();
+    function checkWin (board, marker) {
         let gameWon = null;
-        const activePlayerMarker = getActivePlayer().marker;
-        const plays = boardWithValues.reduce((acc, elem, index) => {
-            return elem === activePlayerMarker ? acc.concat(index) : acc;
+        const plays = board.reduce((acc, elem, index) => {
+            return elem === marker ? acc.concat(index) : acc;
         }, [])
         const winConditions = [
             [0,1,2],
@@ -147,13 +110,15 @@ function GameController(
             [2,4,6]
         ]
 
-        if(board.getEmptyCells().length === 0 && gameWon === null) {
+        const emptyCells = board.filter(cell => typeof cell === 'number');
+
+        if(emptyCells.length === 0 && gameWon === null) {
             gameWon = 'tie';
         }
 
         for(const [index, win] of winConditions.entries()) {
             if(win.every(elem => plays.indexOf(elem) > -1)) {
-                gameWon = {index : index, player : activePlayer};
+                gameWon = {index : index, player : getActivePlayer()};
             }
         }
         return gameWon;
@@ -165,16 +130,97 @@ function GameController(
             return 'This is a Tie Match!';
         } else if(check) {
             winner = check.player;
-            console.log(`${check.player} wins this Match!`);
-            return `${check.player} wins this Match!`;
+            console.log(`${check.player.name} wins this Match!`);
+            return `${check.player.name} wins this Match!`;
         } else {
             winner = null;
         }
     }
 
+    const findBotMove = (board, marker) => {
+        const origBoard = board;
+        const botMarker = marker;
+        const playerMarker = players[0].isBot ? players[1].marker : players[0].marker;
+
+
+        // minmax algorithm for bot
+        const minmax = (board, marker) => {
+            // get empty cells
+            const emptyCellIndices = board.filter(cell => typeof cell === 'number');
+
+            // terminal states
+            if(checkWin(board, botMarker)) {
+                return {score: 10};
+            } else if(checkWin(board, playerMarker)) {
+                return {score: -10};
+            } else if(emptyCellIndices.length === 0) {
+                return {score: 0};
+            }
+
+            // array to store all the test plays
+            const allTestPlays = [];
+
+            // loop through all the empty cells
+            for(let i = 0 ; i < emptyCellIndices.length; i ++) {
+
+                //current test play
+                const currentTestPlay = {};
+                currentTestPlay.index = board[emptyCellIndices[i]];
+
+                //make a move
+                board[emptyCellIndices[i]] = marker;
+
+                //another moves until reaches terminal states
+                if(marker === botMarker) {
+                    const result = minmax(board, playerMarker);
+                    currentTestPlay.score = result.score;
+                } else {
+                    const result = minmax(board, botMarker);
+                    currentTestPlay.score = result.score;
+                }
+
+                //reset the board
+                board[emptyCellIndices[i]] = currentTestPlay.index;
+
+                //push current test play to the all test plays
+                allTestPlays.push(currentTestPlay);
+            }
+
+            // find the best play
+            let bestScore, bestPlayIndex;
+            if(marker === botMarker) {
+                bestScore = -Infinity;
+                for(let i = 0; i < allTestPlays.length; i++) {
+                    if(allTestPlays[i].score > bestScore) {
+                        bestScore = allTestPlays[i].score;
+                        bestPlayIndex = i;
+                    }
+                }
+            } else {
+                bestScore = Infinity;
+                for(let i = 0; i < allTestPlays.length; i++) {
+                    if(allTestPlays[i].score < bestScore) {
+                        bestScore = allTestPlays[i].score;
+                        bestPlayIndex = i;
+                    }
+                }
+            }
+
+            return allTestPlays[bestPlayIndex];
+        }
+
+        return minmax(origBoard, botMarker).index;
+    }
+
+    const playBotMove = () => {
+        const move = findBotMove(board.printBoard() ,getActivePlayer().marker);
+        console.log(move);
+        playRound(move);
+    }
+
     printNewRound();
 
-    return {getActivePlayer, playRound, getBoard: board.getBoard, getWinner};
+    return {getActivePlayer, playRound, getBoard: board.getBoard, resetBoard: board.resetBoard, getWinner};
 }
 
 function ScreenController() {
@@ -186,9 +232,11 @@ function ScreenController() {
     const gameContainer = document.querySelector('.game-container');
     const playerOneInput = document.querySelector('#playerOne');
     const playerTwoInput = document.querySelector('#playerTwo');
+    const isPlayerOneBot = document.querySelector('#bot-x');
+    const isPlayerTwoBot = document.querySelector('#bot-o');
 
     startGameBtn.addEventListener('click', () => {
-        game = GameController(playerOneInput.value || undefined, playerTwoInput.value || undefined);
+        game = GameController(playerOneInput.value || undefined, playerTwoInput.value || undefined, isPlayerOneBot.checked, isPlayerTwoBot.checked);
         console.log(playerOneInput, playerTwoInput)
         gameContainer.style.display = 'block';
         updateScreen();
@@ -197,19 +245,26 @@ function ScreenController() {
     const updateScreen = () => {
         boardDiv.textContent = "";
 
-        const board = game.getBoard();
+        const boardWithValues = game.getBoard();
         const activePlayer = game.getActivePlayer();
 
         playerTurnDiv.textContent = `It's ${activePlayer.name}'s turn.`;
 
         result.textContent = '';
-        if(game.getWinner() === 'tie') result.textContent = `This game is a Tie !`;
-        else if(game.getWinner()) result.textContent = `${game.getWinner().name} wins this match !`;
-
-        board.forEach(cell => {
+        
+        if(game.getWinner() === 'tie') {
+            result.textContent = `This game is a Tie !`;
+            game.resetBoard();
+        }
+        else if(game.getWinner()) {
+            result.textContent = `${game.getWinner().name} wins this match !`;
+            game.resetBoard();
+        }
+        
+        boardWithValues.forEach(cell => {
             const cellButton = document.createElement('button');
             cellButton.classList.add('cell');
-            cellButton.dataset.index = board.indexOf(cell);
+            cellButton.dataset.index = boardWithValues.indexOf(cell);
             cellButton.textContent = typeof cell.getValue() !== 'number' ? cell.getValue() : '';
             boardDiv.appendChild(cellButton);
         })
@@ -228,5 +283,4 @@ function ScreenController() {
 }
 
 
-// ScreenController();
-const game = GameController('Kev', 'Bot', false, true);
+ScreenController()
